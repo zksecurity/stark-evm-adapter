4d198d897307115468d89c390f7e9c118c02da50
mellowcroc <mellowcroc@gmail.com>
2024-07-24 16:16:19 +0900

    fix: update to correct contract addresses

--- examples/verify_stone_proof.rs
+++ examples/verify_stone_proof.rs
@@ -2,9 +2,9 @@ use ethers::{
     contract::ContractError,
     core::k256::ecdsa::SigningKey,
     middleware::SignerMiddleware,
-    providers::{Http, Middleware, Provider, ProviderError},
+    providers::{Http, Middleware, Provider},
     signers::{LocalWallet, Signer, Wallet},
-    types::{Address, U256, U64},
+    types::{Address, U64},
     utils::{hex, Anvil},
 };
 use stark_evm_adapter::{
@@ -79,7 +79,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // start verifying all split proofs
     println!("Verifying trace decommitments:");
-    let contract_address = Address::from_str("0x5899Efea757E0Dbd6d114b3375C23D7540f65fa4").unwrap();
+    let contract_address = Address::from_str("0x634dcf4f1421fc4d95a968a559a450ad0245804c").unwrap();
     for i in 0..split_proofs.merkle_statements.len() {
         let key = format!("Trace {}", i);
         let trace_merkle = split_proofs.merkle_statements.get(&key).unwrap();
@@ -90,7 +90,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     println!("Verifying FRI decommitments:");
-    let contract_address = Address::from_str("0x3E6118DA317f7A433031F03bB71ab870d87dd2DD").unwrap();
+    let contract_address = Address::from_str("0xdef8a3b280a54ee7ed4f72e1c7d6098ad8df44fb").unwrap();
     for (i, fri_statement) in split_proofs.fri_merkle_statements.iter().enumerate() {
         let call = fri_statement.verify(contract_address, signer.clone());
 
@@ -100,7 +100,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (_, continuous_pages) = split_proofs.main_proof.memory_page_registration_args();
 
     let memory_fact_registry_address =
-        Address::from_str("0xFD14567eaf9ba941cB8c8a94eEC14831ca7fD1b4").unwrap();
+        Address::from_str("0x40864568f679c10ac9e72211500096a5130770fa").unwrap();
 
     for (index, page) in continuous_pages.iter().enumerate() {
         let register_continuous_pages_call =
